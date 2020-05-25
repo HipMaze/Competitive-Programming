ef10742de1cbb5708a2cf375387edd8abfdf597d
Ayman Azm <40792340+Ayman-Azm@users.noreply.github.com>
2020-05-25 09:19:14 +0100

    Add files via upload
    Easy first problem to start the day (25/05)

---
+++ 4A-watermelon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if (x%2==0 && x>3)
+    {
+        printf("YES");
+    }else
+    {
+        printf("NO");
+    }
+    return 0;
+}
